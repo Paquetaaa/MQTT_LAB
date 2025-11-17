ae91120357e73b5da1a93e1906a4ab298501080c
Paquetaaa <121290992+Paquetaaa@users.noreply.github.com>
2025-11-17 15:58:58 +0100

    Add files via upload

---
+++ TP1_1client.py
@@ -0,0 +1,53 @@
+import paho.mqtt.client as mqtt
+import random
+import time
+
+message = [
+    "Bonjour tout le monde!",
+    "Bienvenue dans le monde de MQTT.",
+    "Ce message est envoyé via un broker MQTT.",
+    "MQTT est un protocole léger de messagerie.",
+    "Il est souvent utilisé dans les applications IoT.",
+    "Les messages sont publiés sur des topics.",
+    "Les clients peuvent s'abonner à ces topics.",
+    "Cela permet une communication asynchrone.",
+    "Merci d'avoir lu ces messages!",
+    "Au revoir et à bientôt!"
+]
+
+
+def on_connect(client, userdata, flags, rc):
+  print("Connected with result code "+str(rc))
+  client.subscribe("hello")
+
+def on_message(client, userdata, msg):
+    print(msg.payload.decode())
+    client.disconnect()
+
+def connect_mqtt():
+   client.connect("localhost",1883,60)
+
+
+def send_message():
+    for i in range(10):
+        temps = random.randint(1,5)
+        client.publish("hello", message[i])
+        time.sleep(temps)
+
+
+
+
+
+if __name__ == "__main__":
+    client = mqtt.Client()
+
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    connect_mqtt()
+
+    send_message()
+
+    client.loop_forever()
+
+
