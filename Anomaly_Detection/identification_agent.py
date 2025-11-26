256189e9db9d5a8ce9df34b750ff7c4296dac6fb
Paquetaaa <121290992+Paquetaaa@users.noreply.github.com>
2025-11-26 13:42:36 +0100

    Refactor print statements for alert messages

--- Anomaly_Detection/identification_agent.py
+++ Anomaly_Detection/identification_agent.py
@@ -10,22 +10,22 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     alert_msg = msg.payload.decode() ## Message d'alerte reçu
-    print("\n=== ALERTE REÇUE ===")
-    print("Topic :", msg.topic)
-    print("Message :", alert_msg)
+    print("ALERTE REÇUE")
+    print("Topic:", msg.topic)
+    print("Message:", alert_msg)
 
     ## On récupère le sensor qui pose problème à partir du topic d'alerte
 
     # extraction zone/type/id
     parts = msg.topic.split("/")
     if len(parts) != 4:
-        print("Format de topic inattendu, ignoré.")
+        print("Format de topic inattendu : ignoré.")
         return
 
     zone, datatype, sensor_id, _ = parts
 
     reset_topic = f"{zone}/{datatype}/{sensor_id}/reset"
-    print("→ Envoi d'un reset au capteur :", sensor_id)
+    print("Envoi d'un reset au capteur :", sensor_id)
 
     client.publish(reset_topic, "reset")
 
@@ -45,7 +45,7 @@ if __name__ == "__main__":
     client.on_message = on_message
     client.connect("localhost", 1883, 60)
 
-    print("Identification agent démarré…")
+    print("Identification agent démarré")
     client.loop_forever()
 
 
